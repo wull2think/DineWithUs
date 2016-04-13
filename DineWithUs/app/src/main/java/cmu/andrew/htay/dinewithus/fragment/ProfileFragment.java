d8a25b4e6d725494a660c1105f314f13b63eed00
nsundara93 <nsundara@andrew.cmu.edu>
2016-04-13 14:08:29 -0400

    Profile stuff done, with dummy functions

--- DineWithUs/app/src/main/java/cmu/andrew/htay/dinewithus/fragment/ProfileFragment.java
+++ DineWithUs/app/src/main/java/cmu/andrew/htay/dinewithus/fragment/ProfileFragment.java
@@ -16,9 +16,11 @@ public class ProfileFragment extends Fragment {
 
     private EditText like1EditText, like2EditText, like3EditText;
     private EditText dislike1EditText, dislike2EditText, dislike3EditText;
+    private EditText phoneEditText, emailEditText;
 
     private String like1, like2, like3;
     private String dislike1, dislike2, dislike3;
+    private String phone, email;
 
     public static ProfileFragment newInstance() {
         Bundle args = new Bundle();
@@ -49,6 +51,9 @@ public class ProfileFragment extends Fragment {
         dislike2EditText = (EditText) v.findViewById(R.id.dislike2EditText);
         dislike3EditText = (EditText) v.findViewById(R.id.dislike3EditText);
 
+        phoneEditText = (EditText) v.findViewById(R.id.phoneEditText);
+        emailEditText = (EditText) v.findViewById(R.id.emailEditText);
+
         like1EditText.addTextChangedListener(likeWatcher1);
         like2EditText.addTextChangedListener(likeWatcher2);
         like3EditText.addTextChangedListener(likeWatcher3);
@@ -57,29 +62,33 @@ public class ProfileFragment extends Fragment {
         dislike2EditText.addTextChangedListener(dislikeWatcher2);
         dislike3EditText.addTextChangedListener(dislikeWatcher3);
 
+        phoneEditText.addTextChangedListener(phoneWatcher);
+        emailEditText.addTextChangedListener(emailWatcher);
+
         return v;
+    }
+
+    private void saveProfile() {
+        return;
     }
 
     private TextWatcher likeWatcher1 = new TextWatcher()
     {
-        // called when the user enters a number
+        // called when the user edits like1 field
         @Override
         public void onTextChanged(CharSequence s, int start,
                                   int before, int count)
         {
-            // convert billEditText's text to a double
             try
             {
                 like1 = s.toString();
+                saveProfile();
             } // end try
             catch (NumberFormatException e)
             {
                 like1 = "American"; // default if an exception occurs
+                saveProfile();
             } // end catch
-
-            // update the standard and custom tip EditTexts
-            //hiEditText.setText(String.format("%d", quiz2));
-            // updateStandard(); // update the 10, 15 and 20% EditTexts
         } // end method onTextChanged
 
         @Override
@@ -92,28 +101,25 @@ public class ProfileFragment extends Fragment {
                                       int after)
         {
         } // end method beforeTextChanged
-    }; // end quiz1Watcher
+    };
 
     private TextWatcher likeWatcher2 = new TextWatcher()
     {
-        // called when the user enters a number
+        // called when the user edits like2 field
         @Override
         public void onTextChanged(CharSequence s, int start,
                                   int before, int count)
         {
-            // convert billEditText's text to a double
             try
             {
                 like2 = s.toString();
+                saveProfile();
             } // end try
             catch (NumberFormatException e)
             {
                 like2 = "American"; // default if an exception occurs
+                saveProfile();
             } // end catch
-
-            // update the standard and custom tip EditTexts
-            //hiEditText.setText(String.format("%d", quiz2));
-            // updateStandard(); // update the 10, 15 and 20% EditTexts
         } // end method onTextChanged
 
         @Override
@@ -126,28 +132,25 @@ public class ProfileFragment extends Fragment {
                                       int after)
         {
         } // end method beforeTextChanged
-    }; // end quiz1Watcher
+    };
 
     private TextWatcher likeWatcher3 = new TextWatcher()
     {
-        // called when the user enters a number
+        // called when the user edits like3 field
         @Override
         public void onTextChanged(CharSequence s, int start,
                                   int before, int count)
         {
-            // convert billEditText's text to a double
             try
             {
                 like3 = s.toString();
+                saveProfile();
             } // end try
             catch (NumberFormatException e)
             {
                 like3 = "American"; // default if an exception occurs
+                saveProfile();
             } // end catch
-
-            // update the standard and custom tip EditTexts
-            //hiEditText.setText(String.format("%d", quiz2));
-            // updateStandard(); // update the 10, 15 and 20% EditTexts
         } // end method onTextChanged
 
         @Override
@@ -160,5 +163,160 @@ public class ProfileFragment extends Fragment {
                                       int after)
         {
         } // end method beforeTextChanged
-    }; // end quiz1Watcher
+    };
+
+    private TextWatcher dislikeWatcher1 = new TextWatcher()
+    {
+        // called when user edits the dislike1 field
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count)
+        {
+            try
+            {
+                dislike1 = s.toString();
+                saveProfile();
+            } // end try
+            catch (NumberFormatException e)
+            {
+                dislike1 = "Canadian"; // default if an exception occurs
+                saveProfile();
+            } // end catch
+        } // end method onTextChanged
+
+        @Override
+        public void afterTextChanged(Editable s)
+        {
+        } // end method afterTextChanged
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after)
+        {
+        } // end method beforeTextChanged
+    };
+
+    private TextWatcher dislikeWatcher2 = new TextWatcher()
+    {
+        // called when user edits the dislike2 field
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count)
+        {
+            try
+            {
+                dislike1 = s.toString();
+                saveProfile();
+            } // end try
+            catch (NumberFormatException e)
+            {
+                dislike1 = "Canadian"; // default if an exception occurs
+                saveProfile();
+            } // end catch
+        } // end method onTextChanged
+
+        @Override
+        public void afterTextChanged(Editable s)
+        {
+        } // end method afterTextChanged
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after)
+        {
+        } // end method beforeTextChanged
+    };
+
+    private TextWatcher dislikeWatcher3 = new TextWatcher()
+    {
+        // called when user edits the dislike3 field
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count)
+        {
+            try
+            {
+                dislike1 = s.toString();
+                saveProfile();
+            } // end try
+            catch (NumberFormatException e)
+            {
+                dislike1 = "Canadian"; // default if an exception occurs
+                saveProfile();
+            } // end catch
+        } // end method onTextChanged
+
+        @Override
+        public void afterTextChanged(Editable s)
+        {
+        } // end method afterTextChanged
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after)
+        {
+        } // end method beforeTextChanged
+    };
+
+    private TextWatcher phoneWatcher = new TextWatcher()
+    {
+        // called when user edits the dislike3 field
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count)
+        {
+            try
+            {
+                phone = s.toString();
+                saveProfile();
+            } // end try
+            catch (NumberFormatException e)
+            {
+                phone = "412-303-0054"; // default if an exception occurs
+                saveProfile();
+            } // end catch
+        } // end method onTextChanged
+
+        @Override
+        public void afterTextChanged(Editable s)
+        {
+        } // end method afterTextChanged
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after)
+        {
+        } // end method beforeTextChanged
+    };
+
+    private TextWatcher emailWatcher = new TextWatcher()
+    {
+        // called when user edits the dislike3 field
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count)
+        {
+            try
+            {
+                email = s.toString();
+                saveProfile();
+            } // end try
+            catch (NumberFormatException e)
+            {
+                email = "nsundara@andrew.cmu.edu"; // default if an exception occurs
+                saveProfile();
+            } // end catch
+        } // end method onTextChanged
+
+        @Override
+        public void afterTextChanged(Editable s)
+        {
+        } // end method afterTextChanged
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after)
+        {
+        } // end method beforeTextChanged
+    };
 }
